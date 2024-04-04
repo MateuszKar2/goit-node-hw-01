d4639de13139dab682434acb656c4676b4186836
MateuszKar2 <system505@protonmail.com>
2024-04-04 14:05:54 +0200

    my work plan

---
+++ contact.js
@@ -0,0 +1,6 @@
+//1)
+// import fs path
+
+// contectsPath = kontakt do plik contact.json ( metody modułu path)
+
+// dodać funkcję do pracy ze zbiorem kontaktów ( moduł fs oraz metody readFile() i writeFile() + eksport funkcji przez module.exports)
---
+++ index.js
@@ -0,0 +1,12 @@
+//2)
+// import pliku contact.js
+
+//import pakietu yargs LUB moduł Commander
+
+//funkcja invokeAction(wykorzystać)
+
+//3)
+//uruchom polecenia w terminalu i zrobić screenshotty
+
+//odpowiednie odnośniki dodać do pliku Readme
+
