af368c5e44a22d5186c31cba26de17328ff63999
MateuszKar2 <system505@protonmail.com>
2024-04-06 12:53:09 +0200

    add file

--- contact.js
+++ contact.js
@@ -1,6 +1,33 @@
 //1)
-// import fs path
 
 // contectsPath = kontakt do plik contact.json ( metody modułu path)
 
 // dodać funkcję do pracy ze zbiorem kontaktów ( moduł fs oraz metody readFile() i writeFile() + eksport funkcji przez module.exports)
+
+
+const { fs } = require('fs').promises;
+const path = require('path');
+
+console.log("Hello everybody all")
+
+
+const contactsPath = (data, path) => {
+    try {
+        fs.writeFile(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+contactsPath(__dirname);
+
+module.exports = contactsPath;
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -10,3 +10,34 @@
 
 //odpowiednie odnośniki dodać do pliku Readme
 
+console.log('jestem');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+    .option("-a, --action <type>", "choose action") 
+
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case "list":
+
+        break;
+
+        case "get":
+
+        break;
+
+        case "add":
+
+        break;
+
+        case "remove":
+
+        break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(argv)
